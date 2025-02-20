a6b8a817ff7aabafc0c26b439ec945ef5b6e4bc5
Berkant Gunes <gunesberkant96@gmail.com>
2025-02-20 20:03:50 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace login_form
+{
+    partial class Login_form
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.usrtxt = new System.Windows.Forms.TextBox();
+            this.passtxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(147, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(42, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Admin Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "UserName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password";
+            // 
+            // usrtxt
+            // 
+            this.usrtxt.Location = new System.Drawing.Point(147, 126);
+            this.usrtxt.Name = "usrtxt";
+            this.usrtxt.Size = new System.Drawing.Size(232, 22);
+            this.usrtxt.TabIndex = 4;
+            // 
+            // passtxt
+            // 
+            this.passtxt.Location = new System.Drawing.Point(147, 180);
+            this.passtxt.Name = "passtxt";
+            this.passtxt.PasswordChar = '*';
+            this.passtxt.Size = new System.Drawing.Size(232, 22);
+            this.passtxt.TabIndex = 5;
+            // 
+            // Login_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 416);
+            this.Controls.Add(this.passtxt);
+            this.Controls.Add(this.usrtxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Login_form";
+            this.Text = "Login_form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox usrtxt;
+        private System.Windows.Forms.TextBox passtxt;
+    }
+}
+
---
+++ Menu.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login_form
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void newAccountToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            newAccount newacc = new newAccount();
+            newacc.MdiParent = this;
+            newacc.Show();
+        }
+
+        private void updateSearchAccountToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UpdationForm up = new UpdationForm();
+            up.MdiParent = this;
+            up.Show();
+        }
+
+        private void allCustomersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CustomerList cl = new CustomerList();
+            cl.MdiParent = this;
+            cl.Show();
+        }
+
+        private void depositToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreditForm crdt = new CreditForm();
+            crdt.MdiParent = this;
+            crdt.Show();
+        }
+
+        private void withdrawToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DebitForm de = new DebitForm();
+            de.MdiParent = this;
+            de.Show();
+        }
+
+        private void transferToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TransferForm tf = new TransferForm();
+            tf.MdiParent = this;
+            tf.Show();
+        }
+
+        private void fDFormToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FDForm fds = new FDForm();
+            fds.MdiParent = this;
+            fds.Show();
+        }
+
+        private void balanceSheetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BalanceSheet bls = new BalanceSheet();
+            bls.MdiParent = this;
+            bls.Show();
+        }
+
+        private void viewFDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ViewFD viewFD = new ViewFD();
+            viewFD.MdiParent = this;
+            viewFD.Show();
+        }
+
+        private void logoutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void changePasswordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
